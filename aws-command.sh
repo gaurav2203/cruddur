0a22751356f73edf8d03d67ed1130f7dd88a7e49
Gaurav Tank <gauravtank2203@gmail.com>
2025-01-20 23:57:36 +0000

    aws sns added

--- aws-command.sh
+++ aws-command.sh
@@ -1,7 +1,9 @@
-# Creatin USD10 budget with alerts
+# Creating USD10 budget with alerts
 aws budgets create-budget \
     --account-id $AWS_ACCOUNT_ID \
     --budget file://aws/json/budget.json \
     --notifications-with-subscribers file://aws/json/subscribers.json
 
-# 
+# Creating SNS 
+aws sns create-topic --name budget-alarm
+aws sns subscribe --topic-arn arn:aws:sns:ca-central-1:904233096914:budget-alarm --protocol email --notification-endpoint gaurav22.tank@gmail.com
