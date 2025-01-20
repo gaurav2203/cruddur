f784aa119bde991f587ba3e8a9175836173da795
Gaurav Tank <gauravtank2203@gmail.com>
2025-01-20 23:37:06 +0000

    creating aws budget

---
+++ aws-command.sh
@@ -0,0 +1,7 @@
+# Creatin USD10 budget with alerts
+aws budgets create-budget \
+    --account-id $AWS_ACCOUNT_ID \
+    --budget file://aws/json/budget.json \
+    --notifications-with-subscribers file://aws/json/subscribers.json
+
+# 
